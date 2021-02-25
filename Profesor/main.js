4a3c8e5940fa9d4a7063adab2b1f3b1f530453c7
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-02-24 19:01:56 -0500

    24_02_2021

--- Profesor/main.js
+++ Profesor/main.js
@@ -31,75 +31,75 @@
 // Suma, resta, multiplicacion, division
 
 
-// const Calculadora = new Object({
-//     sumar(){
-//         for(let value of this.data){
-//             this.res += value;
-//         }
-//         return `${this.menSuma} ${this.res}`;
-//     },
-//     restar(){
-//         for (let i = (this.data.length-1); i >= 0; i--) {
-//             if(this.res==0){
-//                 this.res = this.data[i];
-//             }else{
-//                 this.res -= this.data[i];
-//             }
-//         }
-//         return `${this.menResta} ${this.res}`;
-//     },
-//     multiplicacion(){
-//         for(let value of this.data){
-//             this.res *= value;
-//         }
-//         return `${this.menMultiplicacion} ${this.res}`;
-//     },
-//     dividir(){
-//         for(let value of this.data){
-//             this.res += value;
-//         }
-//         return `${this.menDivicion} ${(this.res)/this.data.length}`;
-//     }
+const Calculadora = new Object({
+    sumar(){
+        for(let value of this.data){
+            this.res += value;
+        }
+        return `${this.menSuma} ${this.res}`;
+    },
+    restar(){
+        for (let i = (this.data.length-1); i >= 0; i--) {
+            if(this.res==0){
+                this.res = this.data[i];
+            }else{
+                this.res -= this.data[i];
+            }
+        }
+        return `${this.menResta} ${this.res}`;
+    },
+    multiplicacion(){
+        for(let value of this.data){
+            this.res *= value;
+        }
+        return `${this.menMultiplicacion} ${this.res}`;
+    },
+    dividir(){
+        for(let value of this.data){
+            this.res += value;
+        }
+        return `${this.menDivicion} ${(this.res)/this.data.length}`;
+    }
     
-// });
-// const Mayor = function(){
-//     let orden = this.x.sort((a, b)=>{return a-b});
-//     let comparacion = parseInt(orden.slice((orden.length)-1, (orden.length)).join());
-//     const resultados = [];
-//     if(comparacion>=5){
-//         resultados.push(Calculadora.sumar.bind({data : orden, menSuma: this.menSuma, res : 0})());
-//         resultados.push(Calculadora.restar.bind({data : orden, menResta: this.menResta, res  : 0})());
-//         resultados.push(Calculadora.multiplicacion.bind({data : orden, menMultiplicacion: this.menMultiplicacion, res : 1})());
-//         resultados.push(Calculadora.dividir.bind({data : orden, menDivicion: this.menDivicion, res : 0})());
-//         this.x.unshift(this.menDatos);
-//         resultados.push(this.x);
-//         return resultados;
-//     }else{
-//         resultados.push(`${this.menMayor} ${comparacion}`);
-//         this.x.unshift(this.menDatos);
-//         resultados.push(this.x);
-//         return resultados;
-//     }
-// }
-// const data = new Object({
-//     x : [],
-//     menSuma : `La suma de los datos son:`,
-//     menResta : `La resta de los datos son:`,
-//     menMultiplicacion : `La Multiplicacion de los datos son:`,
-//     menDivicion : `El promedio de los datos es:`,
-//     menMayor : `El numero mayor es:`,
-//     menDatos : `Los datos suminostrados son`
-// });
-// parar:
-// do{
-//     if(data.x.length <5){
-//         data.x.push(parseInt(prompt("Ingrese un numero")));
-//         continue parar;
-//     }else{
-//         console.log(new Object({Resultado: Mayor.call(data)}).Resultado);
-//         break parar;
-//     }
-// }while(true)
+});
+const Mayor = function(){
+    let orden = this.x.sort((a, b)=>{return a-b});
+    let comparacion = parseInt(orden.slice((orden.length)-1, (orden.length)).join());
+    const resultados = [];
+    if(comparacion>=5){
+        resultados.push(Calculadora.sumar.bind({data : orden, menSuma: this.menSuma, res : 0})());
+        resultados.push(Calculadora.restar.bind({data : orden, menResta: this.menResta, res  : 0})());
+        resultados.push(Calculadora.multiplicacion.bind({data : orden, menMultiplicacion: this.menMultiplicacion, res : 1})());
+        resultados.push(Calculadora.dividir.bind({data : orden, menDivicion: this.menDivicion, res : 0})());
+        this.x.unshift(this.menDatos);
+        resultados.push(this.x);
+        return resultados;
+    }else{
+        resultados.push(`${this.menMayor} ${comparacion}`);
+        this.x.unshift(this.menDatos);
+        resultados.push(this.x);
+        return resultados;
+    }
+}
+const data = new Object({
+    x : [],
+    menSuma : `La suma de los datos son:`,
+    menResta : `La resta de los datos son:`,
+    menMultiplicacion : `La Multiplicacion de los datos son:`,
+    menDivicion : `El promedio de los datos es:`,
+    menMayor : `El numero mayor es:`,
+    menDatos : `Los datos suminostrados son`
+});
+parar:
+do{
+    if(data.x.length <5){
+        data.x.push(parseInt(prompt("Ingrese un numero")));
+        continue parar;
+    }else{
+        console.log(new Object({Resultado: Mayor.call(data)}).Resultado);
+        break parar;
+    }
+}while(true)
 
 
 
